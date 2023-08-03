e90f0e8ac4a19b1df006fb3353b884d1ed5dbefd
RVE8430_hca <rve8430@hca.corpad.net>
2023-08-03 15:30:20 +0000

    Clean-up Code

--- examples/services/image-http-grayscale/src/main.rs
+++ examples/services/image-http-grayscale/src/main.rs
@@ -64,6 +64,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("Application Started");
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+
     let make_svc = make_service_fn(|_| {
         async move {
             Ok::<_, Infallible>(service_fn(move |req| {
@@ -71,9 +72,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             }))
         }
     });
+
     let server = Server::bind(&addr).serve(make_svc);
+
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
+    
     Ok(())
 }
